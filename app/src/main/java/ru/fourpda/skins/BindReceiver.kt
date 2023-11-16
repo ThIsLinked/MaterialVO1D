0d6bd8908a9a5beead22cc476b7a92fe4ccd53ea
ThIsLinked <kolyalitvintsev@gmail.com>
2023-11-16 03:56:44 +0200

    Upload file in the repo.

---
+++ app/src/main/java/ru/fourpda/skins/BindReceiver.kt
@@ -0,0 +1,11 @@
+package ru.fourpda.skins
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BindReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+    }
+
+}
